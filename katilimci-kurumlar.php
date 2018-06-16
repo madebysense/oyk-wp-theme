42433732d2e7d362e8c2c2079fe41c89be275cba
Uğur Arıcı <m.ugurarici@gmail.com>
2018-06-16 03:15:48 +0300

    Katılımcı Kurumlar içeriği tema içine alınır

--- katilimci-kurumlar.php
+++ katilimci-kurumlar.php
@@ -5,24 +5,131 @@ Template Name: Katılımcı Kurumlar
 ?>
 
 <?php get_header(); ?>
-
 <div class="page">
   <div class="container">
     <div class="row">
-      <article class="page-content col-md-offset-2 col-md-8">
+      <article class="page-content supporting-institutions col-md-offset-2 col-md-8">
         <h1 class="page-content-title"><?php the_title(); ?></h1>
         <?php if (function_exists('the_breadcrumb')) the_breadcrumb(); ?>
-        <?php if ( have_posts() ) :
-          while ( have_posts() ) :
-            the_post();
-            the_content();
-            ?>
-          <?php endwhile; else: ?>
-          <p><?php _e('Sonuç Bulunamadı.'); ?></p>
-        <?php endif; ?>
+        <h2 class="text-center">Organizasyon</h2>
+        <div class="row text-center">
+          <div class="col-sm-4">
+            <a href="http://www.lkd.org.tr/" target="_blank" title="Türkiye Linux Kullanıcıları Derneği">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/tlkd.jpg" alt="Türkiye Linux Kullanıcıları Derneği" />
+            </a>
+          </div>
+          <div class="col-sm-4">
+            <a href="http://www.ibu.edu.tr/tr" target="_blank" title="Abant İzzet Baysal Üniversitesi">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/aibu.jpg" alt="Abant İzzet Baysal Üniversitesi" />
+            </a>
+          </div>
+          <div class="col-sm-4">
+            <a href="https://www.pardus.org.tr/" target="_blank" title="Pardus">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/pardus.jpg" alt="Pardus" />
+            </a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-6 col-sm-3">
+            <a href="http://www.inetd.org.tr/" target="_blank" title="İnternet Teknolojileri Derneği">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/inetd.jpg" alt="İnternet Teknolojileri Derneği" />
+            </a>
+          </div>
+          <div class="col-xs-6 col-sm-3">
+            <a href="https://nic.tr" target="_blank" title="nic.tr">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/nictr.jpg" alt="nic.tr" />
+            </a>
+          </div>
+          <div class="col-xs-6 col-sm-3">
+            <a href="http://www.agyoneticileri.org/" target="_blank" title="Ağ Yöneticileri Derneği">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/ayd.jpg" alt="Ağ Yöneticileri Derneği" />
+            </a>
+          </div>
+          <div class="col-xs-6 col-sm-3">
+            <a href="http://www.bmo.org.tr/" target="_blank" title="Bilgisayar Mühendisleri Odası">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/bmo.jpg" alt="Bilgisayar Mühendisleri Odası" />
+            </a>
+          </div>
+        </div>
+        <hr>
+        <h2 class="text-center">Sınıf Sponsorları</h2>
+        <div class="row text-center">
+          <div class="col-sm-4">
+            <a href="http://hipo.biz/" target="_blank" title="Hipo">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/hipo.jpg" alt="Hipo" />
+            </a>
+          </div>
+          <div class="col-sm-4">
+            <a href="https://ozguryazilim.com.tr" target="_blank" title="Özgür Yazılım A.Ş.">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/oyas.jpg" alt="Özgür Yazılım A.Ş." />
+            </a>
+          </div>
+          <div class="col-sm-4">
+            <a href="https://mono.net.tr" target="_blank" title="Mono Bilişim">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/mono.jpg" alt="Mono Bilişim" />
+            </a>
+          </div>
+        </div>
+        <hr>
+        <h2 class="text-center">Eğitmen Sağlayan Kurumlar</h2>
+        <div class="row">
+          <div class="col-xs-6 col-sm-3">
+            <a href="https://genomize.com/" target="_blank" title="Genomize">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/genomize.jpg" alt="Genomize" />
+            </a>
+          </div>
+          <div class="col-xs-6 col-sm-3">
+            <a href="https://ozguryazilim.com.tr" target="_blank" title="Özgür Yazılım A.Ş.">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/oyas.jpg" alt="Özgür Yazılım A.Ş." />
+            </a>
+          </div>
+          <div class="col-xs-6 col-sm-3">
+            <a href="https://www.invictuseurope.com" target="_blank" title="Invictus">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/invictus.jpg" alt="Invictus" />
+            </a>
+          </div>
+          <div class="col-xs-6 col-sm-3">
+            <a href="https://www.innoverabt.com" target="_blank" title="Innovera">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/innovera.jpg" alt="Innovera" />
+            </a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-6 col-sm-3">
+            <a href="http://lab2023.com" target="_blank" title="Lab2023">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/lab2023.jpg" alt="Lab2023" />
+            </a>
+          </div>
+          <div class="col-xs-6 col-sm-3">
+            <a href="https://kodgemisi.com" target="_blank" title="Kod Gemisi">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/kodgemisi.jpg" alt="Kod Gemisi" />
+            </a>
+          </div>
+          <div class="col-xs-6 col-sm-3">
+            <a href="http://bilkent.edu.tr" target="_blank" title="Bilkent Üniversitesi">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/bilkent-universitesi.jpg" alt="Bilkent Üniversitesi" />
+            </a>
+          </div>
+          <div class="col-xs-6 col-sm-3">
+            <a href="https://mbs.ist" target="_blank" title="Made by Sense">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/madebysense.jpg" alt="Made by Sense" />
+            </a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-6 col-sm-3 col-sm-offset-3">
+            <a href="http://ulakbim.tubitak.gov.tr" target="_blank" title="TÜBİTAK ULAKBİM">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/tubitak-ulakbim.jpg" alt="TÜBİTAK ULAKBİM" />
+            </a>
+          </div>
+          <div class="col-xs-6 col-sm-3">
+            <a href="http://www.msb.gov.tr" target="_blank" title="Milli Savunma Bakanlığı">
+              <img class="img-thumbnail img-responsive" src="<?php bloginfo("template_url"); ?>/assets/images/destekleyen-kurumlar/milli-savunma-bakanligi.jpg" alt="Milli Savunma Bakanlığı" />
+            </a>
+          </div>
+        </div>
       </article>
     </div>
   </div>
 </div>
-
 <?php get_footer(); ?>
